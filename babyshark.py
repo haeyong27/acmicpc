f91a5c1f4df40b8fb519186eff286b41af07bfdc
Haeyong Hwang <41499598+haeyong27@users.noreply.github.com>
2019-03-31 16:35:44 +0900

    아기상어 업데이트
    우선순위 큐 사용해서 다시 풀기

--- babyshark.py
+++ babyshark.py
@@ -1,47 +1,73 @@
-# T = int(input())
-# for testcase in range(1, T+1):
-#   mapp.append(map(int, list(input().split(' '))))
-# 
-#     
-s ='''4 3 2 1
-0 0 0 0
-0 0 9 0
-1 2 3 4'''
-n = 4
+# n = int(input()) 
+# mapp = []
+# for i in range(n):
+#     mapp.append(list(map(int, input().split(' '))))
+
+s ='''5 4 3 2 3 4
+4 3 2 3 4 5
+3 2 9 5 6 6
+2 1 2 3 4 5
+3 2 1 6 5 4
+6 6 6 6 6 6'''
+
+n = len(s.split('\n'))
+
 mapp = []
 for i in s.split('\n'):
     mapp.append(list(map(int, i.split(' '))))
 
+shark_loc = []
 for i in range(n):
     for j in range(n):
         if mapp[i][j] == 9:
-            shark_loc = (i, j)
+            shark_loc.append((i, j))
+            mapp[i][j] = 0
 
-print(shark_loc)
 
-#함수를 만들어야하는데 일차적으로는 현재 먹을 수 있는 물고기들의 위치를 가져온다. 
-# 먹어야할 마리수를 체크하고 (2마리 라고 가정) 두번 움직인 후 레벨을 올리고 다시 목록을 불러온다.
 shark_level = 2
-fish = []
+count = 0
+dx = [-1, 0, 0, 1]
+dy = [0, -1, 1, 0]
+check = [[0 for _ in range(n)] for _ in range(n)]
+dist = [[0 for _ in range(n)] for _ in range(n)]
+answer = 0
 
-def eat():
-    pass
+while(shark_loc):
 
-def find_fish(level, loc, mapp):
-    for i in range(n):
-        for j in range(n):
-            if mapp[i][j] < level:
-                fish.append((i, j))
-    # 2개를 먹어야하니까 먹을 2개를 선정
-    # 거리따져보고, 조건따져보고 2개 선정하기
-    # 그리고 마지막에 먹은 위치를 shark_loc에 대입
-    # 움직인 시간 체크하고
-
+    flag = 0
+    x, y = shark_loc.pop(0)
     
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<n and 0<=ny<n and shark_level > mapp[nx][ny] and mapp[nx][ny] != 0:
+            answer += 1 + dist[x][y]
+            check = [[0 for _ in range(n)] for _ in range(n)]
+            dist = [[0 for _ in range(n)] for _ in range(n)]
+            #잡아먹음
+            mapp[nx][ny] = 0
+            count += 1
+            if shark_level == count:
+                shark_level+=1
+                count = 0
+            #그 장소로 이동 
+            #shark_loc에 추가
+            #그다음에 다시 처음으로 가기
+            shark_loc = [(nx, ny)]
+            flag = 1
+            break
+
+    if flag == 0:
+        for i in range(4):   
+            nx = x + dx[i]
+            ny = y + dy[i] 
+            if 0<=nx<n and 0<=ny<n and (shark_level == mapp[nx][ny] or mapp[nx][ny] == 0) and check[nx][ny] == 0:
+                shark_loc.append((nx, ny))
+                dist[nx][ny] = dist[x][y] + 1
+                check[nx][ny] = 1
+    # for i in mapp:
+    # print(i)
 
 
-    
-    
 
-
-
+print(answer)
