11e5a00dc99c39d768dd7df69a97d490d2ab1feb
Haeyong Hwang <41499598+haeyong27@users.noreply.github.com>
2019-04-02 22:58:21 +0900

    나무 재테크 -> if문 유무로 시간비교해보기

--- treeinvestment.py
+++ treeinvestment.py
@@ -19,24 +19,21 @@ for i in trees:
     x, y, z = i
     treemap[x][y].append(z)
 year = 0
+flag = 0
 while (True):
     treetokill = []
     # 봄
     # 양분 소비
     for i in range(n):
         for j in range(n):
-            for treeage in sorted(treemap[i][j]):
-                if yangboonmap[i][j] - treeage >= 0:
-                    yangboonmap[i][j] -= treeage
-                else:  
-                    #양분을 소비하지 못하는 나무의 좌표와 나이
-                    treetokill.append((i, j, treeage))
-
-    # 나이 한살 먹기
-    for i in range(n):
-        for j in range(n):
             if treemap[i][j]:
-                treemap[i][j] = [a + 1 for a in treemap[i][j]]
+                for treeage in sorted(treemap[i][j]):
+                    if yangboonmap[i][j] - treeage >= 0:
+                        yangboonmap[i][j] -= treeage
+                    else:  
+                        #양분을 소비하지 못하는 나무의 좌표와 나이
+                        treetokill.append((i, j, treeage))
+
 
     # 여름은 죽을 나무가 죽고, 양분이 되기
     for i, j, c in treetokill:
@@ -44,21 +41,56 @@ while (True):
         yangboonmap[i][j] += int(c/2)
 
 
+    # 나이 한살 먹기
+    for i in range(n):
+        for j in range(n):
+            if treemap[i][j]:
+                treemap[i][j] = [a + 1 for a in treemap[i][j]]
 
 
+
+
+
+    # if (year+1)==k:
+    #     ans = 0
+    #     flag = 1   
+    #     for i in range(n):
+    #         for j in range(n):
+    #             ans += len(treemap[i][j])
+
+    #     # 가을은 번식
+    #     for i in range(n):
+    #         for j in range(n):
+    #             if treemap[i][j]:
+    #                 for treeage in sorted(treemap[i][j]):
+    #                     if treeage % 5 == 0:
+    #                         for ii in [-1, 0, 1]:
+    #                             for jj in [-1, 0, 1]:
+    #                                 if ii == 0 and jj == 0:
+    #                                     continue
+    #                                 ni = i + ii
+    #                                 nj = j + jj
+    #                                 if 0 <= ni < n and 0 <= nj < n:
+    #                                     ans += 1
+        
+
+    if flag == 1:
+        print(ans)
+        break
     # 가을은 번식
     for i in range(n):
         for j in range(n):
-            for treeage in sorted(treemap[i][j]):
-                if treeage % 5 == 0:
-                    for ii in [-1, 0, 1]:
-                        for jj in [-1, 0, 1]:
-                            if ii == 0 and jj == 0:
-                                continue
-                            ni = i + ii
-                            nj = j + jj
-                            if 0 <= ni < n and 0 <= nj < n:
-                                treemap[ni][nj].append(1)
+            if treemap[i][j]:
+                for treeage in sorted(treemap[i][j]):
+                    if treeage % 5 == 0:
+                        for ii in [-1, 0, 1]:
+                            for jj in [-1, 0, 1]:
+                                if ii == 0 and jj == 0:
+                                    continue
+                                ni = i + ii
+                                nj = j + jj
+                                if 0 <= ni < n and 0 <= nj < n:
+                                    treemap[ni][nj].append(1)
 
     # 겨울에 양분뿌리기
     for i in range(n):
