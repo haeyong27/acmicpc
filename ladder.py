5b760212e141f0cf9e457fd81a126b7f87bdc7bf
Haeyong Hwang <41499598+haeyong27@users.noreply.github.com>
2019-04-06 00:20:47 +0900

    사다리 조작

--- ladder.py
+++ ladder.py
@@ -1,56 +1,64 @@
 n, m, h = map(int, input().split(' '))
-mapp  = [[0 for _ in range(n)] for _ in range(h)]
-
+mapp  = [[0 for _ in range(n*2)] for _ in range(h)]
 
 for _ in range(m):
     x, y = map(int, input().split(' '))
-    mapp[x-1][y-1] = 1
-    mapp[x-1][y] = 1
+    mapp[x-1][y*2] = 1
 
-#다리를 놓을 수 있는 좌표 뽑기
-ladder = []
-for i in range(h):
-    for j in range(n-1):
-        if mapp[i][j] == 0 and mapp[i][j+1] == 0:
-            ladder.append((i, j))
 
-len_ladder = len(ladder)
-ladder_check = [0 for _ in range(len_ladder)]
 
-def check():
-    for i in range(n):
-        tempi = i
-        for idx in range(h):
-            if mapp[idx][tempi] == 1 and tempi < n - 1  and mapp[idx][tempi+1] == 1:
-                    tempi += 1
-            elif mapp[idx][tempi] == 1 and mapp[idx][tempi-1] == 1:
-                    tempi -= 1
-        if tempi != i:
+
+
+def check_map():
+    for y in range(1, n*2, 2):
+        tempy = y
+        for x in range(h):
+            if y == 2*(n-1) + 1:
+                if mapp[x][y-1] == 1:
+                    y -=2
+            elif mapp[x][y+1] == 1:
+                y += 2
+            elif mapp[x][y-1] == 1:
+                y -= 2
+        if tempy != y:
             return False
     return True
 
+if check_map():
+    print(0)
+    exit()
+#사다리 설치할 수 있는 위치 뽑아내기
+install_list = []
+for x in range(h):
+    for y in range(2,n*2,2):
+        if mapp[x][y] == 1:
+            continue
+        if y == 2*(n-1):
+            if mapp[x][y-2] == 1:
+                continue
+        elif mapp[x][y-2] == 1 or mapp[x][y+2] == 1:
+            continue
+        install_list.append((x, y))
 
-def dfs(idx, cnt, a):
-    if cnt == a:
-        if check():
-            for i in mapp:
-                print(i)
+checklist = [0 for _ in range(len(install_list))]
+
+def dfs(idx, cnt, k):
+
+    if cnt == k:
+        if check_map():
+            print(k)
             exit()
         return
-        
-    for i in range(idx, len_ladder):
-        if ladder_check[i] == 1:
+
+    for i in range(len(install_list)):
+        if checklist[i] == 1:
             continue
-        x, y = ladder[i]
-        if mapp[x][y] == 1 or mapp[x][y+1] == 1:
-            continue
+        checklist[i] = 1
+        x, y = install_list[i]
         mapp[x][y] = 1
-        mapp[x][y+1] = 1
-        ladder_check[i] = 1
-        dfs(i, cnt + 1, a)
+        dfs(i, cnt+1, k)
+        checklist[i] = 0
         mapp[x][y] = 0
-        mapp[x][y+1] = 0
-        ladder_check[i] = 0
 
 def main():
     for i in [1, 2, 3]:
@@ -58,4 +66,3 @@ def main():
     return -1
 
 print(main())
-
