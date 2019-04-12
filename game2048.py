2bd300c767605820ee04b852f189c7b2477016c3
Haeyong Hwang <41499598+haeyong27@users.noreply.github.com>
2019-04-12 23:51:59 +0900

    2048

--- game2048.py
+++ game2048.py
@@ -4,137 +4,88 @@ ori_map = []
 for i in range(n):
     ori_map.append(list(map(int, input().split(' '))))
 
+mapp = []
+
+def merge():
+    global mapp
+    complete_map = []
+
+    for i in mapp:
+        # 0제거한 리스트
+        temp_list = [item for item in i if item != 0]
+        for i in range(1, len(temp_list)):
+            if temp_list[i] == temp_list[i-1]:
+                temp_list[i-1] *= 2
+                temp_list[i] = 0
+
+        temp_list = [item for item in temp_list if item != 0]
+        complete_map.append(temp_list[:])
+    mapp = []
+    for i in complete_map:
+        mapp.append(i+[0 for _ in range(n-len(i))])
+
+
+def leftright():
+    global mapp
+    for i in mapp:
+        i.reverse()
+
+
+def updown():
+    global mapp
+    for i in range(n):
+        for j in range(n):
+            if i<j:
+                mapp[i][j], mapp[j][i] = mapp[j][i], mapp[i][j]
+
+def total(d):
+    global mapp
+    
+    #left
+    if d==0:
+        merge()
+    #right
+    if d==1:
+        leftright()
+        merge()
+        leftright()
+    #up
+    if d==2:
+        updown()
+        merge()
+        updown()
+    #down
+    if d == 3:
+        updown()
+        leftright()
+        merge()
+        leftright()
+        updown()
+
+
 ans = 0
-def dfs(mapp, cnt, d):
+temp = []
+def dfs(cnt):
+    global temp
     global ans
-    if cnt == n:
+    global mapp
+    if cnt == 5:
+        mapp = []
+        for i in ori_map:
+            mapp.append(i[:])
+
+        for i in temp:
+            total(i)
+
         for i in mapp:
             result = max(i)
             ans = max(ans, result)
         return
 
-    if d == 0:
-        #왼쪽으로 기울기
-        for i in range(n):
-            temp = mapp[i][0]
-            idx = 0
-            for j in range(1, n):
-                if mapp[i][j] == 0:
-                    continue
-                if temp == mapp[i][j]:
-                    mapp[i][idx] = temp*2
-                    temp = 0
-                    mapp[i][j] = 0
-                    idx += 1
-                else:
-                    mapp[i][idx] = temp
-                    idx += 1
-                    temp = mapp[i][j]
+    for i in range(4):
+        temp.append(i)
+        dfs(cnt+1)
+        temp.pop()
 
-        
-        for i in range(n):
-            idx = 0
-            for j in range(n):
-                if mapp[i][j] != 0:
-                    temp = mapp[i][j]
-                    mapp[i][j] = 0
-                    mapp[i][idx] = temp
-                    idx += 1
-
-        for i in range(4):
-            dfs(mapp, cnt+1, i)
-
-    if d == 1:
-        #왼쪽으로 기울기
-        for i in range(n):
-            temp = mapp[i][n-1]
-            idx = n-1
-            for j in reversed(range(n-1)):
-                if mapp[i][j] == 0:
-                    continue
-                if temp == mapp[i][j]:
-                    mapp[i][idx] = temp*2
-                    temp = 0
-                    mapp[i][j] = 0
-                    idx -= 1
-                else:
-                    mapp[i][idx] = temp
-                    idx -= 1
-                    temp = mapp[i][j]
-
-
-        for i in range(n):
-            idx = n-1
-            for j in reversed(range(n)):
-                if mapp[i][j] != 0:
-                    temp = mapp[i][j]
-                    mapp[i][j] = 0
-                    mapp[i][idx] = temp
-                    idx -= 1
-        for i in range(4):
-            dfs(mapp, cnt+1, i)
-
-    if d == 2:
-        
-        for j in range(n):
-            temp = mapp[0][j]
-            idx = 0 
-            for i in range(1, n):
-                if mapp[i][j] == 0:
-                    continue
-                if mapp[i][j] == temp:
-                    mapp[idx][j] = temp*2
-                    idx += 1
-                    mapp[i][j] = 0
-                    temp = 0
-                else:
-                    mapp[idx][j] = temp
-                    temp = mapp[i][j]
-                    idx += 1
-
-        for j in range(n):
-            idx = 0
-            for i in range(n):
-                if mapp[i][j] != 0:
-                    temp = mapp[i][j]
-                    mapp[i][j] = 0
-                    mapp[idx][j] = temp
-                    idx+=1
-
-        for i in range(4):
-            dfs(mapp, cnt+1, i)
-
-    if d == 3:
-        for j in range(n):
-            temp = mapp[n-1][j]
-            idx = n-1
-            for i in reversed(range(n-1)):
-                if mapp[i][j] == 0:
-                    continue
-                if mapp[i][j] == temp:
-                    mapp[idx][j] = temp*2
-                    idx -= 1
-                    mapp[i][j] = 0
-                    temp = 0
-                else:
-                    mapp[idx][j] = temp
-                    temp = mapp[i][j]
-                    idx -= 1
-
-
-        for j in range(n):
-            idx = n-1
-            for i in reversed(range(n)):
-                if mapp[i][j] != 0:
-                    temp = mapp[i][j]
-                    mapp[i][j] = 0
-                    mapp[idx][j] = temp
-                    idx-=1
-
-        for i in range(4):
-            dfs(mapp, cnt+1, i)
-
-            
-for i in range(4):
-    dfs(ori_map, 0, i)
+dfs(0)
 print(ans)
